1efbcde67c94dc0195da24b87a0cf9d85768dc00
Muazzamkhan03 <96993942+Muazzamkhan03@users.noreply.github.com>
2022-07-16 12:50:59 +0500

    Ipdate

--- manager.cpp
+++ manager.cpp
@@ -60,7 +60,7 @@ void Manager::Game(){
     while(!checkWin()){
         maze.Display(player.getX(),player.getY());
         Input();
-        std::cin.ignore(std::numeric_limits<std::streamsize>std::max(),'\n');
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
         Clear();
     }
     std::cout<<"Congrats, you finished the maze\n";

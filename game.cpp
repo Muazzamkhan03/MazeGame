48d3f8a211b25205c90d67c22c4a1a92c9f2c47b
Muazzam Khan <muazzamkhan1802@icloud.com>
2022-07-23 14:05:50 +0500

    Updates

--- game.cpp
+++ game.cpp
@@ -4,7 +4,6 @@ void Game::Play(){
     while(!checkWin()){
         maze.Display(player.getX(),player.getY());
         Input();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
         Clear();
     }
     std::cout<<"Congrats, you finished the maze\n";
